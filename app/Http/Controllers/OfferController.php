fe8748700de5bb4f40acd2b7f4d7da23eeaa5391
Ahmed Saber <devahmedsaber@gmail.com>
2021-04-25 04:20:42 +0200

    Save Image With Ajax

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OfferRequest;
 use App\Models\Offer;
 use App\Traits\OfferTrait;
 use Illuminate\Http\Request;
@@ -15,16 +16,30 @@ class OfferController extends Controller
     }
     public function store(Request $request){
         // Save Offer Into DB Using Ajax
-        //$file_name = $this->saveImage($request->photo, 'images/offers');
+        $file_name = $this->saveImage($request->photo, 'images/offers');
 
         // Insert Data.
-        Offer::create([
-            //'photo' => $file_name,
+        $offer = Offer::create([
+            'photo' => $file_name,
             'name_ar' => $request->name_ar,
             'name_en' => $request->name_en,
             'price' => $request->price,
             'details_ar' => $request->details_ar,
             'details_en' => $request->details_en,
         ]);
+        if ($offer){
+            return response()->json([
+                'status' => true,
+                'msg' => 'تم الحفظ بنجاح',
+
+            ]);
+        }else{
+            return response()->json([
+                'status' => false,
+                'msg' => 'فشل الحفظ برجاء المحاولة مجددآ',
+
+            ]);
+        }
+
     }
 }
--- resources/views/ajaxoffers/create.blade.php
+++ resources/views/ajaxoffers/create.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
     <div class="container">
+
+        <div class="alert alert-success" id="success_msg" style="display: none">
+            تم الحفظ بنجاح
+        </div>
+
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
@@ -14,7 +19,7 @@
                     </div>
                 @endif
                 <br>
-                <form method="POST" action="" enctype="multipart/form-data">
+                <form id="offerForm" method="POST" action="" enctype="multipart/form-data">
                     @csrf
                     <div class="mb-3">
                         <label for="photo" class="form-label">{{ trans('messages.PhotoField') }}</label>
@@ -72,20 +77,19 @@
     <script>
         $(document).on('click', '#saveOffer', function (e){
             e.preventDefault();
+            var formData = new FormData($('#offerForm')[0]);
             $.ajax({
                 type: 'post',
+                enctype: 'multipart/form-data',
                 url: "{{ route('ajax.offers.store') }}",
-                data: {
-                    '_token'     : "{{ csrf_token() }}",
-                    {{-- 'photo'      : $("input[name='photo']").val(), --}}
-                    'name_ar'    : $("input[name='name_ar']").val(),
-                    'name_en'    : $("input[name='name_en']").val(),
-                    'price'      : $("input[name='price']").val(),
-                    'details_ar' : $("input[name='details_ar']").val(),
-                    'details_en' : $("input[name='details_en']").val(),
-                },
+                data: formData,
+                processData: false,
+                contentType: false,
+                cache: false,
                 success: function (data){
-
+                    if (data.status == true) {
+                        $('#success_msg').show();
+                    }
                 },
                 error: function (reject){
 
